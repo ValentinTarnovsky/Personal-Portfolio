ba9b8cd6de0d2a6a154e14cfaaf59a82861f9c3b
Valentin Tarnovsky <tarnovsky.valentin@gmail.com>
2024-05-03 18:09:26 -0300

    Scroll Bar Style Theme Fixed

--- frontend/src/LinkRouter/LinkRouter.jsx
+++ frontend/src/LinkRouter/LinkRouter.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import Portfolio from "../pages/Portfolio/Portfolio";
 import NoPage from "../pages/NoPage/NoPage";
@@ -8,6 +8,14 @@ import MyContext from "../contexts/MyContext";
 
 const LinkRouter = () => {
     const { themeData } = useContext(MyContext);
+
+    useEffect(() => {
+        const root = document.documentElement;
+        root.style.setProperty("--scrollbar-width", "0.375rem");
+        root.style.setProperty("--scrollbar-track-background", themeData === "light" ? "#c8c8c9" : "#0c0f1b");
+        root.style.setProperty("--scrollbar-thumb-background", themeData === "light" ? "#1ca4ff" : "#f4b944");
+        root.style.setProperty("--scrollbar-thumb-border-radius", ".25rem");
+    }, [themeData]);
 
     return (
         <Box
